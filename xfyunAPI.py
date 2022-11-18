8e80212b07a198e7fee2112c11e0ef30d34b604e
Zakkoree <1223105850@qq.com>
2022-11-18 15:24:13 +0800

    Update xfyunAPI.py

--- xfyunAPI.py
+++ xfyunAPI.py
@@ -175,7 +175,10 @@ def on_open(ws):
     thread.start_new_thread(run, ())
 
 
-def asr():
+wsParam = None
+def asr(APPID, APISecret, APIKey, AudioFile):
+    global wsParam
+    wsParam = Ws_Param(APPID = APPID, APISecret = APISecret, APIKey = APIKey, AudioFile = AudioFile)
     websocket.enableTrace(False)
     wsUrl = wsParam.create_url()
     ws = websocket.WebSocketApp(
@@ -187,18 +190,12 @@ def asr():
     else:
         return None
 
-
 if __name__ == "__main__":
     # 测试时候在此处正确填写相关信息即可运行
     time1 = datetime.now()
-    wsParam = Ws_Param(APPID=' ', APISecret=' ',
+    text = asr(APPID=' ', APISecret=' ',
                        APIKey=' ',
                        AudioFile=r'E:\1230.wav')
-    websocket.enableTrace(False)
-    wsUrl = wsParam.create_url()
-    ws = websocket.WebSocketApp(
-        wsUrl, on_message=on_message, on_error=on_error, on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+    print(text)
     time2 = datetime.now()
     print(time2-time1)
