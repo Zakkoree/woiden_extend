57ab374315608ed5e05d4c7e376ed55f5e41c74c
Zakkoree <1223105850@qq.com>
2022-11-21 06:55:43 +0000

    Renew

--- main.py
+++ main.py
@@ -379,11 +379,11 @@ def audioToText(audioFile):
     except:
         logger.error("ASR_CHOICE is not set, skip ASR")
             
-    try:
-        if ASR_CHOICE == 'BAIDU':
-            APP_ID = os.environ['APP_ID']
-            API_KEY = os.environ['API_KEY']
-            SECRET_KEY = os.environ['SECRET_KEY']
+#    try:
+#        if ASR_CHOICE == 'BAIDU':
+#            APP_ID = os.environ['APP_ID']
+#            API_KEY = os.environ['API_KEY']
+#            SECRET_KEY = os.environ['SECRET_KEY']
             return baiduAPI(APP_ID, API_KEY, SECRET_KEY, mp3_change_pcm(audioFile))
 
         elif ASR_CHOICE == 'IBM':
