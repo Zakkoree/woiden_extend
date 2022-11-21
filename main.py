d606de6062be451e87adcbf2275e224b0f148286
Zakkoree <1223105850@qq.com>
2022-11-21 20:34:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ extendRetryNum = 10
 # 续订重试间隔时间（秒）
 intervalTime = 10
 
-additional_information = "有问题附上报错信息到 https://github.com/Zakkoree/woiden_extend/issues 发起issue"
+additional_information = "有问题到 https://github.com/Zakkoree/woiden_extend/issues 发起issue"
 
 logger = Logger(LoggerName="Extend")
 
